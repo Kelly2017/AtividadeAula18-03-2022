835e1d249f6149b7979dd514c9b4074f5d54ce69
Flugelo <gabrielflugel@gmail.com>
2022-03-18 20:31:14 -0400

    media

---
+++ AtividadeAula18-03-2022.php
@@ -0,0 +1,34 @@
+
+
+<html>
+    <head>
+
+        <title>
+            <h1>Média por Aluno</h1>
+        </title>
+    <head>
+
+            <body>
+                <h3>Calculadora média Aluno</h3>
+            </body>
+
+            <form action='home.php' method="get">
+                Nota 1: <input type="number" name="num1"><br>
+                Nota 2: <input type="number" name="num2"><br>
+                Nota 3: <input type="number" name="num3"><br>
+                Nota 4: <input type="number" name="num4"><br>
+                <input type="submit">
+            </form>
+               
+            <?php 
+                $num1 = $_GET['num1'];
+                $num2 = $_GET['num2'];
+                $num3 = $_GET['num3'];
+                $num4 = $_GET['num4'];
+                
+                $media = ($num1 + $num2 + $num3 + $num4)/4;
+                
+                echo "Média: $media";
+            ?>
+
+</html>
